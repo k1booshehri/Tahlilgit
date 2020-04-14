28801eae94dc2c130dd857bd65f63145ed0f36e7
Bunny <hanihaghshenas11@gmail.com>
2020-04-14 06:53:55 +0430

    Connect officList and officeForm and Update dashboard
    -Add 'create new office' to clinicList
    -Update dashboard with localStorage property

--- project-front/src/pages/OfficeList.js
+++ project-front/src/pages/OfficeList.js
@@ -1,66 +1,51 @@
 import React, { Component } from "react";
-import Drofficedata from '../pages/officedata/officedata.json'
-
+import Drofficedata from "../pages/officedata/officedata.json";
+import DrDashboard from "./DrDashboard";
 
 export default class OfficeList extends Component {
-   
-  /* constructor(props){
-       super(props);
-       this.state = {
-           
-           'OfficeList' : []
-       }
-   } 
- componentDidMount (){
-       this.getItems();
+  constructor(props) {
+    super(props);
+    this.state = {
+      OfficeList: [],
+    };
+  }
+  //   componentDidMount() {
+  //     this.getItems();
+  //   }
+  //   getItems() {
+  //     fetch("")
+  //       .then((results) => results.json())
+  //       .then((results) => this.setState({ OfficeList: results }));
+  //   }
 
-   }
-   getItems(){
-       fetch('')
-       .then(results =>results.json())
-       .then(results => this.setState({'OfficeList' : results}))
-   } */
- 
- 
-   render() {
-        return (
-             <div >
-                 
-                 <div >
-                 { Drofficedata.map((postdetail , index)=> {
-                return <h1 >
-                 
-             <div className = "Drofficecard">
-           
-           
-       
-        <div >
-        <div> {postdetail.building_name}</div>
-        </div>
-        <div>
-         
-          <div className = "titleOffice">
-          {postdetail.building_adress}
-          </div>
-          </div>
-        
-
-         
-             
-             </div>
-             
-             
-             </h1>
-             
-            
-             })}
-             <button className="newoffice" > مطب جدید </button>
-             
-             </div> 
-             </div>
-
-            
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        {Drofficedata.map((postdetail, index) => {
+          return (
+            <h1 id="h1">
+              <div className="Drofficecard" id="Drofficecard">
+                <div>
+                  <div> {postdetail.building_name}</div>
+                </div>
+                <div>
+                  <div className="titleOffice" id="titleOffice">
+                    {postdetail.building_adress}
+                  </div>
+                </div>
+              </div>
+            </h1>
+          );
+        })}
+        <button
+          className="newoffice"
+          id="1-1"
+          onClick={(e) => this.props.updateState(e)}
+        >
+          {" "}
+          مطب جدید{" "}
+        </button>
+      </div>
+    );
+  }
 }
