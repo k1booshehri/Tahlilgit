9ef8f1aa0c4dfc2925e744f5086b9f2cc62f27ca
Bunny <hanihaghshenas11@gmail.com>
2020-04-14 07:40:25 +0430

    Resolve conflict error on clinicList.js

--- project-front/src/pages/OfficeList.js
+++ project-front/src/pages/OfficeList.js
@@ -3,59 +3,55 @@ import Drofficedata from "../pages/officedata/officedata.json";
 import DrDashboard from "./DrDashboard";
 
 export default class OfficeList extends Component {
-   
-  constructor(props){
-       super(props);
-       this.state = {
-           
-           'officeList' : []
-       }
-   } 
- componentDidMount (){
-       this.getItems();
-
-   }
-   getItems(){
-       fetch('http://localhost:8000/api/auth/officelist', {
-        method: 'GET',
-        headers:{
-          Authorization: "token " + sessionStorage.getItem("token"),
-          'Content-Type': 'application/json'
-        }
-      })
-       .then(res => res.json())
-       .then(results => this.setState({'officeList' : results}))
-       .catch(error => console.error('Error:', error));
-   } 
+  constructor(props) {
+    super(props);
+    this.state = {
+      officeList: [],
+    };
+  }
+  componentDidMount() {
+    this.getItems();
+  }
+  getItems() {
+    fetch("http://localhost:8000/api/auth/officelist", {
+      method: "GET",
+      headers: {
+        Authorization: "token " + sessionStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((results) => this.setState({ officeList: results }))
+      .catch((error) => console.error("Error:", error));
+  }
   render() {
-        return (
-             <div >
-                 
-                 <div >
-                 { this.state.officeList.map((postdetail , index)=> {
-                return <h1 >
-                 
-             <div className = "Drofficecard">
-       <div >
-        <div> {postdetail.city}</div>
-        </div>
+    return (
+      <div>
         <div>
-            <div className = "titleOffice">
-          {postdetail.address}
-          </div>
-          </div>
-         </div>
-             </h1>
-             })}
-             <button className="newoffice" > مطب جدید </button>
-             
-             </div> 
-             </div>
-
-            
-        );
-    }
-
- 
-  
+          {this.state.officeList.map((postdetail, index) => {
+            return (
+              <h1>
+                <div className="Drofficecard">
+                  <div>
+                    <div> {postdetail.city}</div>
+                  </div>
+                  <div>
+                    <div className="titleOffice">{postdetail.address}</div>
+                  </div>
+                </div>
+              </h1>
+            );
+          })}
+          <button
+            className="newoffice"
+            id="1-1"
+            onClick={(e) => this.props.updateState(e)}
+          >
+            {" "}
+            ثبت مطب جدید{" "}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
